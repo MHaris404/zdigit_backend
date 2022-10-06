610bfd20c130078265963e051ad4e3ec72ef8df0
mharis404 <mharis96.mh@gmail.com>
2022-10-06 14:29:01 +0500

    heroku added http and url np

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ const port = process.env.PORT
 
 
 
-app.listen(proxy.evn.PORT || 5000, function(){
+app.listen(process.env.PORT || 5000, function(){
     console.log("hellow Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
