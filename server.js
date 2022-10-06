c82228a67bd21bc2f322289c83d41ab2166725fc
mharis404 <mharis96.mh@gmail.com>
2022-10-06 13:10:32 +0500

    changes made fro heroku port/listen in server.js function

--- server.js
+++ server.js
@@ -13,4 +13,8 @@ app.use(morgan('dev'));
 require('./app/routes.js')(app, connection); // load our routes
 
 const port = process.env.PORT
-app.listen(port, ()=> logger.success(`Server Started on port ${port} on ` + new Date()))
+//app.listen(port, ()=> logger.success(`Server Started on port ${port} on ` + new Date()))
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
