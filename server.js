b31effceac5b64adda2e7a056d6748b7ea5297e3
mharis404 <mharis96.mh@gmail.com>
2022-10-06 14:07:30 +0500

    heroku added http and url np

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ const port = process.env.PORT
 
 
 
-app.listen(proxy.evn.PORT || 80, function(){
+app.listen(proxy.evn.PORT || 5000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
