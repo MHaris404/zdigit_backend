bc7c27b6b0e4bdef496826db179a3908e5c9c11a
mharis404 <mharis96.mh@gmail.com>
2022-10-07 17:03:11 +0500

    heroku mysql config

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const logger = require('node-color-log');
 const connection = require('./config/dbconnection')
 
 app.use(express.json())
-app.use(cors({origin: true, credentials: true}))
+// app.use(cors({origin: true, credentials: true}))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(morgan('production'));
 
