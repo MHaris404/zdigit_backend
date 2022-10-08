9d03ecd9a75775693ef4f03bf672c6a3bde72b3d
mharis404 <mharis96.mh@gmail.com>
2022-10-08 20:49:34 +0500

    app crash check 2

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require('cors');
 var morgan = require('morgan')
+var timeout = require('connect-timeout')
 
 const logger = require('node-color-log');
 const connection = require('./config/dbconnection')
@@ -15,21 +16,18 @@ app.use(express.json())
 //   next();
 // });
 app.use(cors())
-
 app.use(morgan('production'));
+app.use(timeout('5s'))
 
 require('./app/routes.js')(app, connection); // load our routes
 
-const port = process.env.PORT
+// const port = process.env.PORT
 //app.listen(port, ()=> logger.success(`Server Started on port ${port} on ` + new Date()))
 
 // app.listen(process.env.PORT || 5000, function(){
 //     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 //   });
 
-
 app.listen(process.env.PORT || 5000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
-
-  // app.timeout = 60 * 60 * 60 * 1000;
