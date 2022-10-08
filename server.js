79dc1ab66c645686ea73869ab08b79ece01f4e44
mharis404 <mharis96.mh@gmail.com>
2022-10-08 20:57:15 +0500

    app crash check 5

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+var timeout = express.timeout
 const app = express()
 const cors = require('cors');
 var morgan = require('morgan')
@@ -17,7 +18,13 @@ app.use(express.json())
 // });
 app.use(cors())
 app.use(morgan('production'));
-app.use(timeout('35s'))
+// app.use(timeout('29s'))
+app.use(timeout(120000));
+app.use(haltOnTimedout);
+
+function haltOnTimedout(req, res, next){
+  if (!req.timedout) next();
+}
 
 require('./app/routes.js')(app, connection); // load our routes
 
