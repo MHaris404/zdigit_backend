67538fd43fce6a3d14582c67cba22c344b42bbf4
mharis404 <mharis96.mh@gmail.com>
2022-10-17 17:55:01 +0500

    app update 55

--- app/routes.js
+++ app/routes.js
@@ -13,7 +13,7 @@ module.exports = function (app, connection) {
 	app.get('/check', function (req, res) {
 		res.send({ 
 			status: true, 
-			message: 'backend checkd' 
+			message: 'backend check' 
 		}
 		);
 	});
