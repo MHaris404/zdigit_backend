3325a8c6ad0eaf875165add5c70435d02f6f2f4d
mharis404 <mharis96.mh@gmail.com>
2022-11-23 14:16:37 +0500

    retailo staging poapproval_level 1

--- routes/REST_UPDATE.js
+++ routes/REST_UPDATE.js
@@ -1,38 +1,72 @@
 var connection = require('../config/dbconnection');
 
 exports.updatePOapproval1 = function (req, res) {
-	const {userid,po} = req.body;
+    const { userid, po } = req.body;
 
-	const sqlSearch = "update `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix set pomaster.approval_1 = 'approved' where pomaster.created_by = pomatrix.user_id and pomaster.approval_1 is null and pomaster.rejection_reason_1 is null and pomaster.id = "+ po +" and pomatrix.approver_level_1 = " + userid
-	//const search_query = mysql.format(sqlSearch,[po, userid])
 
-    connection.getConnection( (err, conn) => {
-        if(err) 
-        {
-            console.log("poapproval: " + err + " " + new Date())
+    const sqlSearch0 = "select ifnull(pomaster.approval_1, 0) as approval1 from `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix where pomaster.created_by = pomatrix.user_id and pomaster.approval_1 is not null and pomaster.rejection_reason_1 is null and pomaster.id = ? and pomatrix.approver_level_1 = ?"
+    const sqlSearch0_formatted = mysql.format(sqlSearch0, [userid, po])
+
+    const sqlSearch1 = "update `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix set pomaster.approval_1 = 'approved' where pomaster.created_by = pomatrix.user_id and pomaster.approval_1 is null and pomaster.rejection_reason_1 is null and pomaster.id = ? and pomatrix.approver_level_1 = ?"
+    const sqlSearch1_formatted = mysql.format(sqlSearch1, [userid, po])
+
+    connection.getConnection(async (err, conn) => {
+        if (err) {
+            res.json({
+                status: false,
+                message: err
+            })
         }
-        conn.query (sqlSearch,  (err, result, fields) => {
-            conn.release()
-            if (err)
-                console.log("poapproval inner: " + err + " " + new Date())
+        await conn.query(sqlSearch0_formatted, async (err, result, fields) => {
 
-            if ( result == null | result.length <= 0) {
-                res.json({ //put status
-                    status : false,
-                    message : "failed for a reason"
-                    })
-            } else {
-                console.log("sucess")
-                res.status(200).json({
-                    status : true,
-                    message : `Approved Level 1 of PO# ${po}`
+            console.log("query")
+            console.log(result)
+
+            if (err) {
+                res.json({
+                    status: false,
+                    message: err
                 })
+                conn.release()
+            }
+            if (result == null | result.length <= 0) {
+                res.json({
+                    status: false,
+                    message: "no match found"
+                })
+                
+                conn.release()
+
+            } else {
+                if (result.approval1 == null | result.approval1.length <= 0) {
+
+                } else {
+                    await conn.query(sqlSearch1_formatted, (err, result) => {
+
+                        console.log("inner query")
+                        console.log(result)
+
+                        if (err) {
+                            res.json({
+                                status: false,
+                                message: err
+                            })
+                            conn.release()
+                        } else {
+                            res.status(201).json({
+                                status: true,
+                                message: `Approved Level 1 of PO# ${po} `,
+                                
+                            })
+                        }
+                    })
+                }
             }//end
-            
+
         }) //end of connection.query()
     })
 
 };
-                    
+
 
 
