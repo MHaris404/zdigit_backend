69bd9b98b20cc11f03d4e926f4cd722c0f5651d3
mharis404 <mharis96.mh@gmail.com>
2022-11-23 16:47:10 +0500

    retailo staging poapproval_level 1

--- routes/REST_UPDATE.js
+++ routes/REST_UPDATE.js
@@ -16,13 +16,14 @@ exports.updatePOapproval1 = function (req, res) {
         conn.query(sqlSearch0, function(err, rows, fields) {
             if (err) throw err;
 
-            if (rows[0].approval1.length > 0) {
+            console.log(rows[0])
+            if (rows[0] != null && rows[0].approval1.length > 0) {
                 res.json({
                     status : false,
                     message : "already approved",
                 })
             }
-            if (rows.length <= 0) {
+            if (rows[0].length <= 0) {
                 
                 conn.query(sqlSearch1, (err, result, fields) => {
                     if (err) throw err;
