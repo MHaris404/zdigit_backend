4fce9fc5a0779d9d655d8ae0ebe58d5825f3aba4
mharis404 <mharis96.mh@gmail.com>
2022-11-23 16:39:19 +0500

    retailo staging poapproval_level 1

--- routes/REST_UPDATE.js
+++ routes/REST_UPDATE.js
@@ -8,60 +8,70 @@ exports.updatePOapproval1 = function (req, res) {
     //const sqlSearch0_formatted = mysql.format(sqlSearch0, [userid, po])
 
     const sqlSearch1 = "update `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix set pomaster.approval_1 = 'approved' where pomaster.created_by = pomatrix.user_id and pomaster.approval_1 is null and pomaster.rejection_reason_1 is null and pomaster.id = "+ po +" and pomatrix.approver_level_1 = " + userid
-    const sqlSearch1_formatted = mysql.format(sqlSearch1, [userid, po])
+    //const sqlSearch1_formatted = mysql.format(sqlSearch1, [userid, po])
 
     connection.getConnection( (err, conn) => {
-        if (err) {
-            res.json({
-                status: false,
-                message: err
-            })
-        }
-        conn.query(sqlSearch0, async (err, result, fields) => {
+        if (err) throw err;
 
-            console.log("query")
-            console.log(result)
+        conn.query(sqlSearch0, function(err, rows, fields) {
+            if (err) throw err;
 
-            if (err) {
-                res.json({
-                    status: false,
-                    message: err
-                })
-                conn.release()
-            }
-
-            if ( result == null || result.length <= 0) {
-                
-                conn.query(sqlSearch1, (err, result) => {
-
-                    console.log("inner query")
-                    console.log(result)
-
-                    if (err) {
-                        res.json({
-                            status: false,
-                            message: err
-                        })
-                    } else {
-                        res.status(201).json({
-                            status: true,
-                            message: `Approved Level 1 of PO# ${po} `,
-                            
-                        })
-                    }
-                    
-                    conn.release()
-                })
-            } else if (result != null || result[0].approval1.length == 0) {
+            if (result != null || result[0].approval1.length == 0) {
                 res.json({
                     status : false,
                     message : "already approved",
                 })
+            }
+            if ( result == null || result.length <= 0) {
                 
-                 
-            }//end
+                conn.query(sqlSearch1, (err, result, fields) => {
+                    if (err) throw err;
+                    
+                    res.status(201).json({
+                        status: true,
+                        message: `Approved Level 1 of PO# ${po} `,
+                        
+                    })
+                    
+                })
+            }
+           
+            conn.release();
+        });
 
-        }) //end of connection.query()
+        // conn.query(sqlSearch0, (err, rows, fields) => {
+
+        //     if (result != null || result[0].approval1.length == 0) {
+        //         res.json({
+        //             status : false,
+        //             message : "already approved",
+        //         })
+        //     }
+        //     if ( result == null || result.length <= 0) {
+                
+        //         conn.query(sqlSearch1, (err, result) => {
+
+        //             console.log("inner query")
+        //             console.log(result)
+
+        //             if (err) {
+        //                 res.json({
+        //                     status: false,
+        //                     message: err
+        //                 })
+        //             } else {
+        //                 res.status(201).json({
+        //                     status: true,
+        //                     message: `Approved Level 1 of PO# ${po} `,
+                            
+        //                 })
+        //             }
+                    
+        //             conn.release()
+        //         })
+        //     }
+
+        // }) //end of connection.query()
     })
 
 };
