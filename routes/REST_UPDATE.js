e6946067a029ba1f4921bbdbc40ffae4a710ceb3
mharis404 <mharis96.mh@gmail.com>
2022-11-23 13:22:14 +0500

    retailo staging poapproval_level 1

--- routes/REST_UPDATE.js
+++ routes/REST_UPDATE.js
@@ -6,32 +6,32 @@ app.update("/poapproval_l1", (req, res)=> {
 	const sqlSearch = "update `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix set pomaster.approval_1 = 'approved' where pomaster.created_by = pomatrix.user_id and pomaster.approval_1 is null and pomaster.rejection_reason_1 is null and pomaster.id = ? and pomatrix.approver_level_1 = ?"
 	const search_query = mysql.format(sqlSearch,[po, userid])
 
-	 connection.getConnection( (err, conn) => {
-			if(err) 
-			{
-				console.log("poapproval: " + err + " " + new Date())
-			}
-			conn.query (search_query,  (err, result, fields) => {
-				conn.release()
-				if (err) throw err;
+    connection.getConnection( (err, conn) => {
+        if(err) 
+        {
+            console.log("poapproval: " + err + " " + new Date())
+        }
+        conn.query (search_query,  (err, result, fields) => {
+            conn.release()
+            if (err) throw err;
 
-				if ( result == null || result.length <= 0) {
-					res.json({ //put status
-						status : false,
-						message : "failed for a reason"
-						})
-				} else {
-					console.log("sucess")
-					res.status(200).json({
-						status : true,
-						message : `Approved Level 1 PO # ${po}
-					})
-					
-				}//end
-		}) //end of connection.query()
-		
-	})
-		 
-}) //end of app.post()
+            if ( result == null || result.length <= 0) {
+                res.json({ //put status
+                    status : false,
+                    message : "failed for a reason"
+                    })
+            } else {
+                console.log("sucess")
+                res.status(200).json({
+                    status : true,
+                    message : `Approved Level 1 PO # ${po}`
+                })
+            }//end
+            
+        }) //end of connection.query()
+    })
+
+});
+                    
 
 
