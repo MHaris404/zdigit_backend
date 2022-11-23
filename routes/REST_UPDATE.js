0202b86e133b777b7e8311aaefff1ece6fb6e2a7
mharis404 <mharis96.mh@gmail.com>
2022-11-23 15:59:03 +0500

    retailo staging poapproval_level 1

--- routes/REST_UPDATE.js
+++ routes/REST_UPDATE.js
@@ -5,7 +5,7 @@ exports.updatePOapproval1 = function (req, res) {
     const { userid, po } = req.body;
 
 
-    const sqlSearch0 = "select ifnull(pomaster.approval_1, 0) as approval1 from `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix where pomaster.created_by = pomatrix.user_id and pomaster.approval_1 is not null and pomaster.rejection_reason_1 is null and pomaster.id = "+ po +" and pomatrix.approver_level_1 = " + userid
+    const sqlSearch0 = "select ifnull(pomaster.approval_1, '') as approval1 from `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix where pomaster.created_by = pomatrix.user_id and IfNull(pomaster.approval_1, '') != '' and pomaster.rejection_reason_1 is null and pomaster.id = "+ po +" and pomatrix.approver_level_1 = " + userid
     //const sqlSearch0_formatted = mysql.format(sqlSearch0, [userid, po])
 
     const sqlSearch1 = "update `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix set pomaster.approval_1 = 'approved' where pomaster.created_by = pomatrix.user_id and pomaster.approval_1 is null and pomaster.rejection_reason_1 is null and pomaster.id = "+ po +" and pomatrix.approver_level_1 = " + userid
@@ -30,47 +30,29 @@ exports.updatePOapproval1 = function (req, res) {
                 })
                 conn.release()
             }
-            if (result != null | result.length > 0) {
-                
-                
-                
-                console.log("in")
-                console.log(result.approval1)
-                console.log("in")
-                
-
-                if (result.approval1 != null | result.approval1.length > 0) {
+            if (result != null && result[0].approval1.length == 0) {
                     
-                    await conn.query(sqlSearch1_formatted, (err, result) => {
+                await conn.query(sqlSearch1, (err, result) => {
 
-                        console.log("inner query")
-                        console.log(result)
+                    console.log("inner query")
+                    console.log(result)
 
-                        if (err) {
-                            res.json({
-                                status: false,
-                                message: err
-                            })
-                        } else {
-                            res.status(201).json({
-                                status: true,
-                                message: `Approved Level 1 of PO# ${po} `,
-                                
-                            })
-                        }
-                        
-                        conn.release()
-                    })
+                    if (err) {
+                        res.json({
+                            status: false,
+                            message: err
+                        })
+                    } else {
+                        res.status(201).json({
+                            status: true,
+                            message: `Approved Level 1 of PO# ${po} `,
+                            
+                        })
+                    }
                     
-                    
-                } else {
-                    res.json({
-                        status: false,
-                        message: "asa"
-                    })
                     conn.release()
-                    
-                }
+                })
+                 
             }//end
 
         }) //end of connection.query()
