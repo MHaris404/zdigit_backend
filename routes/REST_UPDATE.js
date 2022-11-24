3e430a57239f2306ea5a80266411c4b048484e7f
mharis404 <mharis96.mh@gmail.com>
2022-11-24 11:53:35 +0500

    retailo staging poapproval

--- routes/REST_UPDATE.js
+++ routes/REST_UPDATE.js
@@ -1,15 +1,15 @@
 var connection = require('../config/dbconnection');
 const mysql = require("mysql2")
 
-exports.updatePOapproval1 = function (req, res) {
+exports.updatePOapproval = function (req, res) {
     const { userid, po } = req.body;
 
-    const sqlSearch0 = "select pomaster.approval_1, pomaster.rejection_reason_1 from `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix where pomaster.created_by = pomatrix.user_id and pomaster.rejection_reason_1 is null and pomaster.id =" + po +" and pomatrix.approver_level_1 = " + userid
-       //const sqlSearch0_formatted = mysql.format(sqlSearch0, [userid, po])
+    const sqlSearch0 = "select pomaster.id ,pomaster.approval_1, pomaster.approval_2, pomaster.rejection_reason_1 ,pomaster.rejection_reason_2 from `0_po_master` pomaster left join `0_user_auth_matrix_for_po` pomatrix on pomaster.created_by = pomatrix.user_id where pomaster.id = "+ po +" and pomatrix.approver_level_1 = "+userid+" or pomatrix.approver_level_2 = " + userid    
+    
+    const sqlSearch1 = "update `0_po_master pomaster` ,`0_user_auth_matrix_for_po` pomatrix set pomaster.approval_1 = 'approved' where pomaster.created_by = pomatrix.user_id and pomaster.approval_1 is null and pomaster.rejection_reason_1 is null and pomaster.approval_2 is null and pomaster.rejection_reason_2 is null and pomaster.id = "+userid+" and pomatrix.approver_level_1 = " + userid 
 
-    const sqlSearch1 = "update `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix set pomaster.approval_1 = 'approved' where pomaster.created_by = pomatrix.user_id and pomaster.approval_1 is null and pomaster.rejection_reason_1 is null and pomaster.id = "+ po +" and pomatrix.approver_level_1 = " + userid
-    //const sqlSearch1_formatted = mysql.format(sqlSearch1, [userid, po])
-
+    const sqlSearch2 = "update `0_po_master pomaster` ,`0_user_auth_matrix_for_po` pomatrix set pomaster.approval_2 = 'approved' where pomaster.created_by = pomatrix.user_id and pomaster.approval_1 is not null and pomaster.rejection_reason_1 is null and pomaster.approval_2 is null and pomaster.rejection_reason_2 is null and pomaster.id = "+userid+" and pomatrix.approver_level_2 = " + userid 
+    
     connection.getConnection( (err, conn) => {
         if (err) throw err;
 
@@ -18,15 +18,21 @@ exports.updatePOapproval1 = function (req, res) {
 
             console.log(rows[0])
 
-            if (rows[0] != null && rows[0].approval_1 =="approved" && rows[0].rejection_reason_1 == null) {
-                res.json({
-                    status : false,
-                    message : `PO# ${po} is already approved at L1`,
+            if (rows[0] != null && rows[0].id == userid && rows[0].approval_1 =="approved" && rows[0].rejection_reason_1 == null && rows[0].approval_2 == null && rows[0].rejection_reason_1 == null)  {
+                
+                conn.query(sqlSearch2, (err, result, fields) => {
+                    if (err) throw err;
+
+                    res.json({
+                        status: true,
+                        message: `PO# ${po} approved at L2`,
+                        code: 2
+                    })
+                    
                 })
 
-                //TODO: check for approval of level2
             }
-            else if (rows[0] != null && rows[0].approval_1 == null && rows[0].rejection_reason_1 == null) {
+            else if (rows[0] != null && rows[0].id == userid && rows[0].approval_1 == null && rows[0].rejection_reason_1 == null) {
                  
                 conn.query(sqlSearch1, (err, result, fields) => {
                     if (err) throw err;
@@ -38,52 +44,43 @@ exports.updatePOapproval1 = function (req, res) {
                     })
                     
                 })
+            }else if (rows[0] != null && rows[0].id == userid && rows[0].approval_1 == null && rows[0].rejection_reason_1 != null) {
+                 
+                conn.query(sqlSearch1, (err, result, fields) => {
+                    if (err) throw err;
+
+                    res.json({
+                        status: false,
+                        message: `PO# ${po} is already rejected at L1`,
+                        code: -1
+                        
+                    })
+                    
+                })
+            }else if (rows[0] != null && rows[0].id != userid ) {
+                 
+                conn.query(sqlSearch1, (err, result, fields) => {
+                    if (err) throw err;
+
+                    res.status(403).json({
+                        status: false,
+                        message: `PO# ${po} cannot be approved by current user at L1`, 
+                        code: 0
+                    })
+                    
+                })
             }else {
                 res.status(401).json({
                     status: false,
                     message: `PO# ${po} cannot be approved by current user at L1`, 
+                    code: 0
                 })
             }
            
             conn.release();
         });
 
-        // conn.query(sqlSearch0, (err, rows, fields) => {
-
-        //     if (result != null || result[0].approval1.length == 0) {
-        //         res.json({
-        //             status : false,
-        //             message : "already approved",
-        //         })
-        //     }
-        //     if ( result == null || result.length <= 0) {
-                
-        //         conn.query(sqlSearch1, (err, result) => {
-
-        //             console.log("inner query")
-        //             console.log(result)
-
-        //             if (err) {
-        //                 res.json({
-        //                     status: false,
-        //                     message: err
-        //                 })
-        //             } else {
-        //                 res.status(201).json({
-        //                     status: true,
-        //                     message: `Approved Level 1 of PO# ${po} `,
-                            
-        //                 })
-        //             }
-                    
-        //             conn.release()
-        //         })
-        //     }
-
-        // }) //end of connection.query()
     })
 
 };
-
-
 
