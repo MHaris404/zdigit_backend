fe5c818f89f561673d51aa12e1e8d8f1b3d2740d
mharis404 <mharis96.mh@gmail.com>
2022-11-25 17:12:18 +0500

    retailo staging poapproval/porejection 1

--- app/routes.js
+++ app/routes.js
@@ -10,204 +10,205 @@ let refreshTokens = []
 
 module.exports = function (app, connection) {
 
-//check
+	//check
 	app.get('/check', function (req, res) {
-		res.send({ 
-			status: true, 
-			message: 'backend check' 
+		res.send({
+			status: true,
+			message: 'backend check'
 		}
 		);
 	});
 
-//check2
-	app.get("/check2", (req,res) => {
+	//check2
+	app.get("/check2", (req, res) => {
 
 		querySQL = "SELECT * FROM 0_users";
 
 		connection.getConnection((err, conn) => {
-			
+
 			//conn.release();
-			if(err) 
-			{
+			if (err) {
 				console.log("check2: " + err)
 			} else {
 				conn.query(querySQL, (error, rows, fields) => {
-					
+
 					if (rows != 0) {
 						res.json({
-								status : true,
-								message : rows
-							})
-					}else {
+							status: true,
+							message: rows
+						})
+					} else {
 						res.json({
-							status : false,
-							message : "no data"
+							status: false,
+							message: "no data"
 						})
 					}
-					
+
 				});
 			}
 		})
 
 	})
 
-//create user
-	app.post("/createuser", (req,res) => {
-		const {name, real_name, role_id, phone} = req.body;
+	//create user
+	app.post("/createuser", (req, res) => {
+		const { name, real_name, role_id, phone } = req.body;
 		const hashedPassword = crypto.createHash('md5').update(utf8.encode(req.body.password)).digest('hex')
 
 		const sqlSearch = "SELECT * FROM 0_users WHERE user_id = ?"
-		const search_query = mysql.format(sqlSearch,[name])
+		const search_query = mysql.format(sqlSearch, [name])
 		var count = "SELECT count(*) FROM 0_users" + 1;
 		const sqlInsert = "INSERT INTO 0_users(id, user_id, password, real_name, role_id, phone) Values (?,?,?,?,?,?)"
-		const insert_query = mysql.format(sqlInsert,[count, name, hashedPassword, real_name, role_id, phone])
+		const insert_query = mysql.format(sqlInsert, [count, name, hashedPassword, real_name, role_id, phone])
 		// ? will be replaced by values
 		// ?? will be replaced by string
-		
+
 		connection.getConnection((err, conn) => {
-			if(err) 
-			{
-				console.log("createuser: "  + err+ " " + new Date())
+			if (err) {
+				console.log("createuser: " + err + " " + new Date())
 			} else {
-				conn.query (search_query, async(err, result, fields) => {
-			
-					if ( result == null || result.length <= 0) {
+				conn.query(search_query, async (err, result, fields) => {
+
+					if (result == null || result.length <= 0) {
 						res.status(409).json({
-							status : false,
-							message : "User already exists"
+							status: false,
+							message: "User already exists"
 						})
-						
+
 						conn.release()
 					} else {
-						await conn.query (insert_query, (err, result)=> {
-							if(err) {
+						await conn.query(insert_query, (err, result) => {
+							if (err) {
 								res.json({
-									status : false,
-									message : err
+									status: false,
+									message: err
 								})
 								throw err
 							} else {
 								res.status(201).json({
-									status : true,
-									message : "Created new User",
-									userid : result.insertId,
+									status: true,
+									message: "Created new User",
+									userid: result.insertId,
 								})
 							}
 						})
-						
+
 					}
-					
+
 				}) //end of connection.query()
 			}
 		})
 
 	}) //end of app.post()
 
-//login user
-	app.post("/login", (req, res)=> {
+	//login user
+	app.post("/login", (req, res) => {
 		const user = req.body.name
 		const company = req.body.company
-		const sqlSearch = "Select * from 0_users where user_id = ?"	
-		const search_query = mysql.format(sqlSearch,[user])
+		const sqlSearch = "Select * from 0_users where user_id = ?"
+		const search_query = mysql.format(sqlSearch, [user])
 
-		 connection.getConnection( (err, conn) => {
-				if(err) 
-				{
-					console.log("login: " + err + " " + new Date())
-				}
-				conn.query (search_query,  (err, result, fields) => {
+		connection.getConnection((err, conn) => {
+			if (err) {
+				console.log("login: " + err + " " + new Date())
+			}
+			conn.query(search_query, (err, result, fields) => {
 				conn.release()
 				if (err) throw err;
 
-				if ( result == null || result.length <= 0) {
+				if (result == null || result.length <= 0) {
 					res.json({ //put status
-						status : false,
-						message : "User does not exist"
-						})	
-							
+						status: false,
+						message: "User does not exist"
+					})
+
 				} else {
 					console.log("sucess")
-					const {id,password, role_id, email} = result[0]
+					const { id, password, role_id, email } = result[0]
 					if (crypto.createHash('md5').update(req.body.password).digest('hex') === password) {
-					
-						const accessToken = generateAccessToken ({user})
-						const refreshToken = generateRefreshToken ({user})
+
+						const accessToken = generateAccessToken({ user })
+						const refreshToken = generateRefreshToken({ user })
 						refreshTokens.push(refreshToken);
 						res.status(200).json({
-							status : true,
-							message : "login successful",
-							details : {
+							status: true,
+							message: "login successful",
+							details: {
 								user,
 								role_id,
 								email,
-								tokens : {accessToken, refreshToken},
+								tokens: { accessToken, refreshToken },
 								company,
 								id
-							}})
+							}
+						})
 					} else {
 						res.json({ //res.status(401)
-							status : false,
-							message : "Password incorrect!",
+							status: false,
+							message: "Password incorrect!",
 						})
 					} //end of pass comparion
 				}//end of User exists i.e. results.length==0
 			}) //end of connection.query()
-			
+
 		})
-			 
+
 	}) //end of app.post()
 
-//refresh a access token
-	app.put("/refreshtoken", (req,res) => {
+	//refresh a access token
+	app.put("/refreshtoken", (req, res) => {
 
-		var {oldvalidrefreshtoken, username} = req.body;
+		var { oldvalidrefreshtoken, username } = req.body;
 
 		if (!refreshTokens.includes(oldvalidrefreshtoken)) {
 			res.status(498).send({
-				status : false,
-				message : "Refresh Token Invalid/ Expired"	
+				status: false,
+				message: "Refresh Token Invalid/ Expired"
 			})
-		}else {
-			refreshTokens = refreshTokens.filter( (c) => c != oldvalidrefreshtoken)
+		} else {
+			refreshTokens = refreshTokens.filter((c) => c != oldvalidrefreshtoken)
 			//remove the old refreshToken from the refreshTokens list
-			const accessToken = generateAccessToken ({user: username})
-			const refreshToken = generateRefreshToken ({user: username})
+			const accessToken = generateAccessToken({ user: username })
+			const refreshToken = generateRefreshToken({ user: username })
 			//generate new accessToken and refreshTokens
 			refreshTokens.push(refreshToken);
 			//save token
 			res.status(200).json({
-				status : false,
-				message : "tokens refresh successful",
-				details : {
-					user : username,
-					tokens : {accessToken, refreshToken}
+				status: false,
+				message: "tokens refresh successful",
+				details: {
+					user: username,
+					tokens: { accessToken, refreshToken }
 				}
 			})
 		}
 	})
 
-//validated content
-	app.get("/posts", validateToken, (req, res)=>{
+	//validated content
+	app.get("/posts", validateToken, (req, res) => {
 		res.send(`${req.user.user} successfully accessed post`)
 	})
 
-//logout
-	app.post("/logout", (req,res)=>{
-	refreshTokens = refreshTokens.filter( (c) => c != req.body.validrefreshtoken)
-	//remove the old refreshToken from the refreshTokens list
-	res.send({ //.status(204)
-		status: true,
-		message: "logout successful"
+	//logout
+	app.post("/logout", (req, res) => {
+		refreshTokens = refreshTokens.filter((c) => c != req.body.validrefreshtoken)
+		//remove the old refreshToken from the refreshTokens list
+		res.send({ //.status(204)
+			status: true,
+			message: "logout successful"
 		})
 	})
 
-//PO Count
-app.get('/pocount', validateToken, REST_GET.getPOCOUNT);
-	
-//PO with Details
-app.get('/podetails', validateToken, REST_GET.getPODETAILS);
+	//PO Count
+	app.get('/pocount', validateToken, REST_GET.getPOCOUNT);
 
-//PO approval level 1
-app.put('/poapproval', validateToken, REST_UPDATE.updatePOapproval);
-	
+	//PO with Details
+	app.get('/podetails', validateToken, REST_GET.getPODETAILS);
+
+	//PO approval level 1,2
+	app.put('/poapproval', validateToken, REST_UPDATE.updatePOapproval);
+
+	//PO rejection level 1,2
+	app.put('/porejection', validateToken, REST_UPDATE.updatePOrejection);
+
 }
--- routes/REST_UPDATE.js
+++ routes/REST_UPDATE.js
@@ -37,7 +37,7 @@ exports.updatePOapproval = function (req, res) {
                 }
 
             }
-            else if (rows[0] != null && rows[0].id == po && rows[0].approval_1 != null && rows[0].rejection_reason_1 == null && rows[0].approval_2 != null && rows[0].rejection_reason_1 == null) {
+            else if (rows[0] != null && rows[0].id == po && rows[0].approval_1 != null && rows[0].rejection_reason_1 == null && rows[0].approval_2 != null && rows[0].rejection_reason_2 == null) {
 
                 res.status(200).json({
                     status: false,
@@ -77,7 +77,8 @@ exports.updatePOapproval = function (req, res) {
                     }).end()
                 }
 
-            } else if (rows[0] != null && rows[0].id == po && rows[0].approval_1 == null && rows[0].rejection_reason_1 != null && rows[0].approval_2 == null && rows[0].rejection_reason_2 == null) {
+            } 
+            else if (rows[0] != null && rows[0].id == po && rows[0].approval_1 == null && rows[0].rejection_reason_1 != null && rows[0].approval_2 == null && rows[0].rejection_reason_2 == null) {
 
                 res.status(200).json({
                     status: false,
@@ -86,7 +87,8 @@ exports.updatePOapproval = function (req, res) {
 
                 }).end()
 
-            } else if (rows[0] != null && rows[0].id != po) {
+            } 
+            else if (rows[0] != null && rows[0].id != po) {
 
                 res.status(401).json({
                     status: false,
@@ -94,7 +96,8 @@ exports.updatePOapproval = function (req, res) {
                     code: 0
                 }).end()
 
-            } else {
+            } 
+            else {
 
                 res.status(401).json({
                     status: false,
@@ -103,6 +106,124 @@ exports.updatePOapproval = function (req, res) {
                     result: rows[0]
                 }).end()
 
+            }
+
+        });
+
+        conn.release();
+
+    })
+
+};
+
+exports.updatePOrejection = function (req, res) {
+    const { userid, po } = req.body;
+
+    const sqlSearch0 = "select pomaster.id ,pomaster.approval_1, pomaster.approval_2, pomaster.rejection_reason_1 ,pomaster.rejection_reason_2, pomatrix.approver_level_1, pomatrix.approver_level_2 from `0_po_master` pomaster left join `0_user_auth_matrix_for_po` pomatrix on pomaster.created_by = pomatrix.user_id where pomaster.id = " + po + " and pomatrix.approver_level_1 = " + userid + " or pomatrix.approver_level_2 = " + userid
+
+    connection.getConnection((err, conn) => {
+        if (err) throw err;
+
+        conn.query(sqlSearch0, function (err, rows, fields) {
+            console.log(rows)
+
+            if (err) throw err;
+
+            else if (rows[0] != null && rows[0].id == po && rows[0].approval_1 == null && rows[0].rejection_reason_1 != null && rows[0].approval_2 == null && rows[0].rejection_reason_2 == null) {
+
+                if (rows[0].approver_level_2 == userid)
+                    
+                    conn.query("update `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix set pomaster.rejection_reason_2 = 'reject' where pomaster.created_by = pomatrix.user_id and pomaster.rejection_reason_1 is not null and pomaster.approval_1 is null and pomaster.rejection_reason_2 is null and pomaster.approval_2 is null and pomaster.id = ? and pomatrix.approver_level_2 = ? ", [po, userid], (err, result, fields) => {
+                        if (err) throw err;
+
+                            res.status(200).json({
+                                status: true,
+                                message: `PO# ${po} rejected at L2`,
+                                code: 1,
+                                level: 2
+                            }).end()
+
+                    })
+                else {
+                    res.status(401).json({
+                        status: false,
+                        message: `PO# ${po} cannot be rejected by current user at L2`,
+                        code: 0
+                    }).end()
+                }
+
+            }
+            
+            else if (rows[0] != null && rows[0].id == po && rows[0].rejection_reason_1 != null && rows[0].approval_1 == null && rows[0].approval_2 == null && rows[0].rejection_reason_2 != null) {
+
+                res.status(200).json({
+                    status: false,
+                    message: `PO# ${po} is already rejected at L2 with reason: ${rows[0].rejection_reason_2}`,
+                    code: -1
+                }).end()
+
+            }
+            else if (rows[0] != null && rows[0].id == po && rows[0].approval_1 != null && rows[0].rejection_reason_1 == null && rows[0].approval_2 != null && rows[0].rejection_reason_2 == null) {
+
+                res.status(200).json({
+                    status: false,
+                    message: `PO# ${po} is already approved at L2}`,
+                    code: -1
+                }).end()
+
+            }
+            else if (rows[0] != null && rows[0].id == po && rows[0].approval_1 == null && rows[0].rejection_reason_1 == null && rows[0].approval_2 == null && rows[0].rejection_reason_2 == null) 
+            {
+
+                if (rows[0].approver_level_1 == userid) {
+                    conn.query("update `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix set pomaster.rejection_reason_1 = 'reject' where pomaster.created_by = pomatrix.user_id and pomaster.approval_1 is null and pomaster.rejection_reason_1 is null and pomaster.approval_2 is null and pomaster.rejection_reason_2 is null and pomaster.id = ? and pomatrix.approver_level_1 = ?", [po, userid], (err, result, fields) => {
+                        if (err) throw err;
+
+                            res.status(200).json({
+                                status: true,
+                                message: `PO# ${po} rejected at L1`,
+                                code: 1,
+                                level: 1
+
+                            }).end()
+                    })
+                }
+                else {
+                    res.status(401).json({
+                        status: false,
+                        message: `PO# ${po} cannot be rejected by current user at L1`,
+                        code: 0
+                    }).end()
+                }
+
+            } 
+            else if (rows[0] != null && rows[0].id == po && rows[0].approval_1 != null && rows[0].rejection_reason_1 == null && rows[0].approval_2 == null && rows[0].rejection_reason_2 == null) 
+            {
+
+                res.status(200).json({
+                    status: false,
+                    message: `PO# ${po} is already approved at L1`,
+                    code: -1
+
+                }).end()
+
+            }
+            else if (rows[0] != null && rows[0].id != po) 
+            {
+                res.status(401).json({
+                    status: false,
+                    message: `PO# ${po} cannot be rejected by current user`,
+                    code: 0
+                }).end()
+            } 
+            else 
+            {
+                res.status(401).json({
+                    status: false,
+                    message: `PO# ${po} cannot be rejected by current user`,
+                    code: 0,
+                    result: rows[0]
+                }).end()
             }
 
         });
