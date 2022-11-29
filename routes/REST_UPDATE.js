9a23757595ebb1d69cc7a41c0493a8656ba366c6
mharis404 <mharis96.mh@gmail.com>
2022-11-29 11:30:44 +0500

    retailo staging app/rej reject reason 4

--- routes/REST_UPDATE.js
+++ routes/REST_UPDATE.js
@@ -12,15 +12,22 @@ exports.updatePOapproval = function (req, res) {
         conn.query(sqlSearch0, function (err, rows, fields) {
             if (err) throw err;
 
+            console.log(userid, po)
             var rowsFiltered =  rows.filter(row => {
                 return row.id == po
             })
+            console.log("FILTERED")
+            console.log(rowsFiltered)
 
             if (rows[0] != null && rowsFiltered[0].id == po && rowsFiltered[0].approval_1 == "approved" && rowsFiltered[0].rejection_reason_1 == null && rowsFiltered[0].approval_2 == null && rowsFiltered[0].rejection_reason_2 == null) {
 
                 if (rowsFiltered[0].approver_level_2 == userid)
                     conn.query("update `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix set pomaster.approval_2 = 'approved' where pomaster.created_by = pomatrix.user_id and pomaster.approval_1 = 'approved' and pomaster.rejection_reason_1 is null and pomaster.approval_2 is null and pomaster.rejection_reason_2 is null and pomaster.id = ? and pomatrix.approver_level_2 = ? ", [po, userid], (err, result, fields) => {
                         if (err) throw err;
+
+                        console.log("RESULTS")
+                        console.log(result)
+                        console.log("RESULT END")
 
                         res.status(200).json({
                             status: true,
