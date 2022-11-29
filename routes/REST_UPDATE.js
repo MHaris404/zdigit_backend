53f47fa99984b265a2be304a63413b2db5bc0d94
mharis404 <mharis96.mh@gmail.com>
2022-11-29 11:53:28 +0500

    retailo staging app/rej reject reason 5

--- routes/REST_UPDATE.js
+++ routes/REST_UPDATE.js
@@ -19,105 +19,117 @@ exports.updatePOapproval = function (req, res) {
             console.log("FILTERED")
             console.log(rowsFiltered)
 
-            if (rows[0] != null && rowsFiltered[0].id == po && rowsFiltered[0].approval_1 == "approved" && rowsFiltered[0].rejection_reason_1 == null && rowsFiltered[0].approval_2 == null && rowsFiltered[0].rejection_reason_2 == null) {
+            if(rowsFiltered.level > 0){
+                if (rows[0] != null && rowsFiltered[0].id == po && rowsFiltered[0].approval_1 == "approved" && rowsFiltered[0].rejection_reason_1 == null && rowsFiltered[0].approval_2 == null && rowsFiltered[0].rejection_reason_2 == null) {
 
-                if (rowsFiltered[0].approver_level_2 == userid)
-                    conn.query("update `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix set pomaster.approval_2 = 'approved' where pomaster.created_by = pomatrix.user_id and pomaster.approval_1 = 'approved' and pomaster.rejection_reason_1 is null and pomaster.approval_2 is null and pomaster.rejection_reason_2 is null and pomaster.id = ? and pomatrix.approver_level_2 = ? ", [po, userid], (err, result, fields) => {
-                        if (err) throw err;
+                    if (rowsFiltered[0].approver_level_2 == userid)
+                        conn.query("update `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix set pomaster.approval_2 = 'approved' where pomaster.created_by = pomatrix.user_id and pomaster.approval_1 = 'approved' and pomaster.rejection_reason_1 is null and pomaster.approval_2 is null and pomaster.rejection_reason_2 is null and pomaster.id = ? and pomatrix.approver_level_2 = ? ", [po, userid], (err, result, fields) => {
+                            if (err) throw err;
 
-                        console.log("RESULTS")
-                        console.log(result)
-                        console.log("RESULT END")
-
-                        res.status(200).json({
-                            status: true,
-                            message: `PO# ${po} approved at L2`,
-                            code: 1,
-                            level: 2
-                        }).end()
-
-                    })
-                else {
-                    res.status(401).json({
-                        status: false,
-                        message: `PO# ${po} cannot be approved by current user at L2`,
-                        code: 0
-                    }).end()
-                }
-
-            }
-            else if (rows[0] != null && rowsFiltered[0].id == po && rowsFiltered[0].approval_1 == "approved" && rowsFiltered[0].rejection_reason_1 == null && rowsFiltered[0].approval_2 == "approved" && rowsFiltered[0].rejection_reason_2 == null) {
-
-                res.status(200).json({
-                    status: false,
-                    message: `PO# ${po} is already approved at L2`,
-                    code: 0
-                }).end()
-            }
-            else if (rows[0] != null && rowsFiltered[0].id == po && rowsFiltered[0].approval_1 == "approved" && rowsFiltered[0].rejection_reason_1 == null && rowsFiltered[0].approval_2 == "rejected" && rowsFiltered[0].rejection_reason_2 != null) {
-
-                res.status(200).json({
-                    status: false,
-                    message: `PO# ${po} is already rejected at L2 with reason: ${rowsFiltered[0].rejection_reason_2}`,
-                    code: -1
-                }).end()
-
-            }
-            else if (rows[0] != null && rowsFiltered[0].id == po && rowsFiltered[0].approval_1 == null && rowsFiltered[0].rejection_reason_1 == null && rowsFiltered[0].approval_2 == null && rowsFiltered[0].rejection_reason_2 == null) {
-
-                if (rowsFiltered[0].approver_level_1 == userid) {
-                    conn.query("update `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix set pomaster.approval_1 = 'approved' where pomaster.created_by = pomatrix.user_id and pomaster.approval_1 is null and pomaster.rejection_reason_1 is null and pomaster.approval_2 is null and pomaster.rejection_reason_2 is null and pomaster.id = ? and pomatrix.approver_level_1 = ?", [po, userid], (err, result, fields) => {
-                        if (err) throw err;
+                            console.log("RESULTS")
+                            console.log(result)
+                            console.log("RESULT END")
 
                             res.status(200).json({
                                 status: true,
-                                message: `PO# ${po} approved at L1`,
+                                message: `PO# ${po} approved at L2`,
                                 code: 1,
-                                level: 1
-
+                                level: 2
                             }).end()
-                    })
+
+                        })
+                    else {
+                        res.status(401).json({
+                            status: false,
+                            message: `PO# ${po} cannot be approved by current user at L2`,
+                            code: 0
+                        }).end()
+                    }
+
                 }
-                else {
-                    res.status(401).json({
+                else if (rows[0] != null && rowsFiltered[0].id == po && rowsFiltered[0].approval_1 == "approved" && rowsFiltered[0].rejection_reason_1 == null && rowsFiltered[0].approval_2 == "approved" && rowsFiltered[0].rejection_reason_2 == null) {
+
+                    res.status(200).json({
                         status: false,
-                        message: `PO# ${po} cannot be approved by current user at L1`,
+                        message: `PO# ${po} is already approved at L2`,
                         code: 0
                     }).end()
                 }
+                else if (rows[0] != null && rowsFiltered[0].id == po && rowsFiltered[0].approval_1 == "approved" && rowsFiltered[0].rejection_reason_1 == null && rowsFiltered[0].approval_2 == "rejected" && rowsFiltered[0].rejection_reason_2 != null) {
 
-            } 
-            else if (rows[0] != null && rowsFiltered[0].id == po && rowsFiltered[0].approval_1 == "rejected" && rowsFiltered[0].rejection_reason_1 != null && rowsFiltered[0].approval_2 == null && rowsFiltered[0].rejection_reason_2 == null) {
+                    res.status(200).json({
+                        status: false,
+                        message: `PO# ${po} is already rejected at L2 with reason: ${rowsFiltered[0].rejection_reason_2}`,
+                        code: -1
+                    }).end()
 
-                res.status(200).json({
-                    status: false,
-                    message: `PO# ${po} is already rejected at L1 with reason: ${rowsFiltered[0].rejection_reason_1}`,
-                    code: -1
+                }
+                else if (rows[0] != null && rowsFiltered[0].id == po && rowsFiltered[0].approval_1 == null && rowsFiltered[0].rejection_reason_1 == null && rowsFiltered[0].approval_2 == null && rowsFiltered[0].rejection_reason_2 == null) {
 
-                }).end()
+                    if (rowsFiltered[0].approver_level_1 == userid) {
+                        conn.query("update `0_po_master` pomaster ,`0_user_auth_matrix_for_po` pomatrix set pomaster.approval_1 = 'approved' where pomaster.created_by = pomatrix.user_id and pomaster.approval_1 is null and pomaster.rejection_reason_1 is null and pomaster.approval_2 is null and pomaster.rejection_reason_2 is null and pomaster.id = ? and pomatrix.approver_level_1 = ?", [po, userid], (err, result, fields) => {
+                            if (err) throw err;
 
-            } 
-            else if (rows[0] != null && rowsFiltered[0].id != po) {
+                                res.status(200).json({
+                                    status: true,
+                                    message: `PO# ${po} approved at L1`,
+                                    code: 1,
+                                    level: 1
 
+                                }).end()
+                        })
+                    }
+                    else {
+                        res.status(401).json({
+                            status: false,
+                            message: `PO# ${po} cannot be approved by current user at L1`,
+                            code: 0
+                        }).end()
+                    }
+
+                } 
+                else if (rows[0] != null && rowsFiltered[0].id == po && rowsFiltered[0].approval_1 == "rejected" && rowsFiltered[0].rejection_reason_1 != null && rowsFiltered[0].approval_2 == null && rowsFiltered[0].rejection_reason_2 == null) {
+
+                    res.status(200).json({
+                        status: false,
+                        message: `PO# ${po} is already rejected at L1 with reason: ${rowsFiltered[0].rejection_reason_1}`,
+                        code: -1
+
+                    }).end()
+
+                } 
+                else if (rows[0] != null && rowsFiltered[0].id != po) {
+
+                    res.status(401).json({
+                        status: false,
+                        message: `PO# ${po} cannot be approved by current user`,
+                        code: 0
+                    }).end()
+
+                    console.log("ROWS")
+                    console.log(rowsFiltered[0])
+
+                } 
+                else {
+
+                    res.status(401).json({
+                        status: false,
+                        message: `PO# ${po} cannot be approved by current user`,
+                        code: 0,
+                        result: rows[0]
+                    }).end()
+
+                }
+            }
+            else{
                 res.status(401).json({
                     status: false,
                     message: `PO# ${po} cannot be approved by current user`,
                     code: 0
                 }).end()
 
-                console.log("ROWS")
+                console.log("ROWS 1")
                 console.log(rowsFiltered[0])
-
-            } 
-            else {
-
-                res.status(401).json({
-                    status: false,
-                    message: `PO# ${po} cannot be approved by current user`,
-                    code: 0,
-                    result: rows[0]
-                }).end()
-
             }
 
         });
